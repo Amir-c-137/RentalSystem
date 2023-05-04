15bb9391d940d56c51ceba0ad30067c485a0533c
trool@LAPTOP-NB6O1EOP.(none) <trool@LAPTOP-NB6O1EOP.(none)>
2023-05-04 22:09:44 +0430

    Customer class added

---
+++ RentalProject/RentalSystem/Customer.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.util.*;
+public class Customer {
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private int id;
+    private List<Rental> rentals;
+
+    public Customer(String name, String email, String phone, String address, int id) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.id = id;
+        rentals = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public List<Rental> getRentals() {
+        return rentals;
+    }
+}
