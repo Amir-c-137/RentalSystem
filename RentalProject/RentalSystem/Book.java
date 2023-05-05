422e5973f8d88b14d06064d8b5e4b35bd21a9e02
trool@LAPTOP-NB6O1EOP.(none) <trool@LAPTOP-NB6O1EOP.(none)>
2023-05-05 15:24:13 +0430

    Book class added

---
+++ RentalProject/RentalSystem/Book.java
@@ -0,0 +1,50 @@
+package org.example;
+import java.util.*;
+public class Book extends Item {
+
+    private String author;
+    private int pages;
+    private boolean isAvailable;
+
+    public Book(String title, String genre, Date releaseDate, String author, int pages, int ID) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.author = author;
+        this.pages = pages;
+        this.id = ID;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+}
