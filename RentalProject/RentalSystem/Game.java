6f8d34650d740df183c861d699579fc83a0442c5
trool@LAPTOP-NB6O1EOP.(none) <trool@LAPTOP-NB6O1EOP.(none)>
2023-05-05 15:45:32 +0430

    Game class added

---
+++ RentalProject/RentalSystem/Game.java
@@ -0,0 +1,44 @@
+package org.example;
+import java.util.*;
+public class Game extends Item {
+    private String platform;
+    private boolean isAvailable;
+
+    public Game(String title, String genre, Date releaseDate, String platform, int Id) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.id = Id;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+}
+
+
